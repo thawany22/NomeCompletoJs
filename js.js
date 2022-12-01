9b2d27e65a1aac717507f65f08409217407fdd6d
Thawany22 <souzaathaay4@gmail.com>
2022-12-01 20:15:31 -0300

    tarefa realizada

---
+++ js.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite o primeiro nome");
+let sobrenome = prompt("Digite seu Sobrenome:");
+
+let nomeCompleto = nome + sobrenome;
+alert(`Seu nome completo é: ${nome + " " + sobrenome} \nSeu nome de catálogo é: ${sobrenome.toUpperCase()}, ${nome}`);
